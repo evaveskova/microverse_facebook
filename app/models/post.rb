41221a2da2f2d033d1a0e206e1f748141e712054
Roy Nyaga <nyagaandreroy@gmail.com>
2019-11-07 23:47:16 +0100

    added associations and validations to the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+	self.primary_key = 'id'
+	
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  validates :content, presence: true, length: { minimum: 2 }
+  # validates :author, presence: true 
 end
