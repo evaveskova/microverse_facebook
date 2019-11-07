f424f8829f3fa07fc74262cb18e1b9cd11e042e9
Roy Nyaga <nyagaandreroy@gmail.com>
2019-11-08 00:00:10 +0100

    added create update destroy show and index actions to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,58 +1,45 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :check_user_for_edit_post, only: %i[ edit ]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
+    @post = Post.new
+    @main_user = User.find(current_user.id)
+    
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
+    @post = Post.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Post has been successfully created"
+      redirect_back(fallback_location: root_path)
+    else
+      render 'posts/index'
+    end    
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "post has been updated"
+      redirect_to root_path
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -61,14 +48,4 @@ class PostsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:id, :author_id, :content)
-    end
 end
