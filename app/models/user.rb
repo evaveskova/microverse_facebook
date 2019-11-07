8f716f36051242fbecdd9bc8d2d79fb0a8dd6378
evaveskova <evaveskova@gmail.com>
2019-11-07 23:42:36 +0100

    added the associations and validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+           :recoverable, :rememberable, :validatable
+    has_many :posts, foreign_key: 'author_id', dependent: :destroy
+
+    validates :first_name, presence: true,  length: { within: 4..20 }
+    validates :last_name, presence: true, length: { within: 4..20 }
+    validates :email, presence: true, format: Devise.email_regexp
+    validates :gender, presence: true, inclusion: { in: %w[male female custom] }
+    validates :birthday, presence: true
+
+    before_save :downcase_email
+    before_save :capitalize_names
+    before_create :gravatar_image_url
 end
