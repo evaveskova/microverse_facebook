d9ea0f6f2ff1bc4ed7b08cfe9b76c7feac443dd8
evaveskova <evaveskova@gmail.com>
2019-11-11 21:00:32 +0100

    added to the user controller, changed the navbar colour

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+    before_action :authenticate_user!
+
+  def index
+      @users = User.all
+  end
+
+  def show
+      @user = User.find(params[:id])
+      @post = Post.new
+  end
+
+end
