7385479cee9da3953f50cec3e7c1858352fbb713
Roy Nyaga <nyagaandreroy@gmail.com>
2019-11-11 20:53:04 +0100

    added more styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-      get "users/index"
+    get "users/index"
     get '/show/:id', to: 'users#show', as: 'user'
     devise_for :users, controllers: {
         registrations: "registrations"
