c19d2385a35c1b6596e097c326fce1ba6b32f454
evaveskova <evaveskova@gmail.com>
2019-11-14 17:59:02 +0100

    added associations and validations for the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+  has_many :likes, as: :likeable, dependent: :destroy
+  validates :content, presence: true
 end
