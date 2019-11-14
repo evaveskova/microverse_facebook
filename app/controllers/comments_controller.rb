cda84b55d6f8aad1dec360d70c3d5c043be40965
Roy Nyaga <nyagaandreroy@gmail.com>
2019-11-14 18:02:46 +0100

    added the create and edit action in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+	def create 
+		@comment = current_user.comments.build(comment_params)
+		if @comment.save 
+			flash[:success] = "comment created"
+			redirect_to root_path
+		else
+			flash[:danger] = "could not be saved"
+			redirect_back(fallback_location: root_path)
+		end 
+	end 
+
+	def edit
+		@comment = Comment.find(params[:id])
+	end 
 end
