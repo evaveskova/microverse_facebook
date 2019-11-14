a9ae708e2b0db951a9f31cc0c8be968c137dfc85
Roy Nyaga <nyagaandreroy@gmail.com>
2019-11-14 22:44:20 +0100

    added test for comment model validations and associations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,39 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(first_name: 'againfirstname', 
+      last_name: "againlastname", 
+      email: 'asuser1@example.com',
+      password: "password",
+      gender: "female",
+      birthday: "2019-11-06"
+      )
+    @post = @user.posts.create(content: "this is my first post")
+    @comment = @user.comments.new(post_id: @post.id, content: "this is my comment")
+  end
+
+  	
+
+  context 'Comment Associations and validations' do
+    it 'All associations must be present' do
+      expect(@comment).to be_valid
+    end
+
+    it 'comment content must be present' do
+    	@comment.content = ""
+      expect(@comment).not_to be_valid
+    end
+
+    it 'comment author must be present' do
+    	@comment.author = nil
+      expect(@comment).not_to be_valid
+    end
+
+    it 'comment post must be present' do
+    	@comment.post = nil
+      expect(@comment).not_to be_valid
+    end
+
+  end 
 end
