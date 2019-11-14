1c2a0d6e7dd12329a90ee6e4266e491ab16d8f22
evaveskova <evaveskova@gmail.com>
2019-11-14 18:10:31 +0100

    added check liker to likes controller, and added before action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action(only: [:create]) { check_liker(current_user) }
+
   def create
     @like = current_user.likes.build(like_params)
       if @like.save
@@ -12,4 +14,14 @@ class LikesController < ApplicationController
   def like_params
       params.require(:like).permit(:likeable_type, :likeable_id)
   end
+
+  def check_liker(current_user)
+    @liking = Like.where(author_id: current_user.id,
+        likeable_type: params[:like][:likeable_type],
+        likeable_id: params[:like][:likeable_id])
+        if @liking.exists?
+          @liking.first.destroy
+            redirect_back(fallback_location: root_path)
+        end
+    end
 end
