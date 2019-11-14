f016ce34c23fe979a0105f6db74e2f1b25018426
evaveskova <evaveskova@gmail.com>
2019-11-14 18:00:13 +0100

    added associations and validations for the likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :likeable, polymorphic: true
-  belongs_to :author
+  belongs_to :author, class_name: 'User', foreign_key: "author_id"
+  validates :author_id, presence: true
+  validates :likeable_type, presence: true
+  validates :likeable_id, presence: true
 end
