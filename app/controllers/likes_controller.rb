36d292be33fa7539e29be01cd27a66e1af349539
evaveskova <evaveskova@gmail.com>
2019-11-14 18:07:46 +0100

    added the create action to the likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.build(like_params)
+      if @like.save
+            flash[:success] = "liked a #{@like.likeable_type}"
+            redirect_back(fallback_location: root_path)
+      end
+    end
+
+    private
+
+  def like_params
+      params.require(:like).permit(:likeable_type, :likeable_id)
+  end
 end
