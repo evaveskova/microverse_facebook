d8c26b13ddaca5917e93ad1ea00f6dbf20344e4b
evaveskova <evaveskova@gmail.com>
2019-11-16 15:47:23 +0100

    added the create method in the friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,10 @@
 class FriendshipsController < ApplicationController
+  def create
+        @friend = User.find(params[:friendship][:friend])
+        unless current_user.friends.include?@friend
+            add_freinds(@friend)
+            flash[:success] = "#{@friend.first_name} has been added to your friend list"
+            redirect_back(fallback_location: root_path)
+        end
+    end
 end
