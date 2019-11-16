80801fe29bea3939a4552c874c3594f9c275530e
evaveskova <evaveskova@gmail.com>
2019-11-16 15:39:38 +0100

    added the associations and validations to the friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend
+  elongs_to :user
+  belongs_to :friend, class_name: 'User'
+
+  validates :user, presence: true
+  validates :friend, presence: true, uniqueness: { scope: :user }
 end
