6dfed39d1fdf4598d1a37b51433e55ffbed034da
evaveskova <evaveskova@gmail.com>
2019-11-20 16:28:18 +0100

    added list of pending friend requests and accepted friends to user controller index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
-    @friendship = Friendship.new
+  	@users = User.all
+  	@friendship = Friendship.new
+  	@confirmed_friends = User.find_confirmed_friends(current_user)
+  	@pending_friends = User.find_pending_friends(current_user)
   end
 
   def show
